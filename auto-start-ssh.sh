ed312833f35d6feabd7c665f441987d043d070da
Daniel Martin <xadlien@gmail.com>
2021-12-24 00:45:30 -0500

    Increase sleep for server at reboot

--- auto-start-ssh.sh
+++ auto-start-ssh.sh
@@ -1,7 +1,7 @@
 EMAIL_ADDR=YOUREMAILHERE
 
 /home/ngrok/bin/ngrok tcp 22 --log=/tmp/ngrok > /dev/null &
-sleep 5
+sleep 10
 API_URL=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url )
 mailx -s "$HOSTNAME NGROK SSH ADDRESS" $EMAIL_ADDR <<< $API_URL
 tail -f /tmp/ngrok
